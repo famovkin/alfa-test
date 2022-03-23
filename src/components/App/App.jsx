9b4719b8f0c4161a3c038d36d50e38f9b34b186b
Tolya Fam <famovkin@yandex.ru>
2022-03-24 01:15:16 +0300

    feat: сверстать Header

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Header from "../Header/Header";
 
 function App() {
   return (
     <div className="app">
       <div className="app__page">
-        {/* <Header /> */}
+        <Header />
         <main className="app__main">
           {/* <Filter />
           <CardsList /> */}
