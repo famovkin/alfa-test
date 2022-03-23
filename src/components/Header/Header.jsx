75a4ce681f8c0b2fc57e78d09b8bcdabd1ade51d
Tolya Fam <famovkin@yandex.ru>
2022-03-24 01:26:29 +0300

    fix: убрать внешнее позиционирование у Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.css";
 
 function Header() {
   return (
-    <header className="header">
+    <header className="header app__header">
       <h1 className="header__title">Rick and Morty Characters</h1>
     </header>
   );
