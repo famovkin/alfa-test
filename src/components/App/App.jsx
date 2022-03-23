b88176bfd4de38624c531d00a5a386ae2e79b168
Tolya Fam <famovkin@yandex.ru>
2022-03-24 01:11:31 +0300

    feat: добавить стили к App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <div className="app__page">
+        {/* <Header /> */}
+        <main className="app__main">
+          {/* <Filter />
+          <CardsList /> */}
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
