75e7807f2ba84d1fc0fd44a3fc60d22f85796ccf
Tolya Fam <famovkin@yandex.ru>
2022-03-24 01:30:50 +0300

    feat: создать класс Api

---
+++ src/utils/Api.js
@@ -0,0 +1,18 @@
+class Api {
+  constructor(baseUrl) {
+    this._baseUrl = baseUrl;
+  }
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Что-то пошло не так. Обратитесь к разработчику`);
+  }
+
+  getCharacters() {
+    return fetch(this._baseUrl).then((res) => this._checkResponse(res));
+  }
+}
+
+export const api = new Api("https://rickandmortyapi.com/api/character/");
