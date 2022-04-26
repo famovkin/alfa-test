70754a9d559ecb61cda1b8c8fc2e5c6350ed8304
Tolya Fam <famovkin@yandex.ru>
2022-04-27 02:41:16 +0300

    feat: add types for cards

---
+++ src/types/card.ts
@@ -0,0 +1,58 @@
+export interface CardState {
+  allCards: Array<Card>;
+  favorites: Array<Card>;
+  isLoaded: boolean;
+}
+
+export enum CardActionTypes {
+  SET_CARDS = "SET_CARDS",
+  UPDATE_FAVORITES = "UPDATE_FAVORITES",
+  DELETE_CARD = "DELETE_CARD",
+  SET_LOADED = "SET_LOADED",
+}
+
+export interface Card {
+  created: string;
+  episode: Array<string>;
+  gender: string;
+  id: number;
+  image: string;
+  location: AdditionalCardInfo;
+  name: string;
+  origin: AdditionalCardInfo;
+  species: string;
+  status: string;
+  type: string;
+  url: string;
+}
+
+interface AdditionalCardInfo {
+  name: string;
+  url: string;
+}
+
+interface SetCardsAction {
+  type: CardActionTypes.SET_CARDS;
+  payload: Array<Card>;
+}
+
+interface UpdateFavoritesAction {
+  type: CardActionTypes.UPDATE_FAVORITES;
+  payload: Card;
+}
+
+export interface DeleteCardAction {
+  type: CardActionTypes.DELETE_CARD;
+  payload: Card;
+}
+
+interface SetLoadedAction {
+  type: CardActionTypes.SET_LOADED;
+  payload: boolean;
+}
+
+export type CardAction =
+  | SetCardsAction
+  | UpdateFavoritesAction
+  | DeleteCardAction
+  | SetLoadedAction;
