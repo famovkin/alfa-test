70ca5690f15abbc348a1f12b718ab2afeddfafa8
Tolya Fam <famovkin@yandex.ru>
2022-04-27 03:57:32 +0300

    feat: update store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,12 @@
 import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from "./reducers/index.ts";
+import rootReducer from "./reducers/index";
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
